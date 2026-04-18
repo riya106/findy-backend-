62a6395fa5cd1c7f8ed376d9bb62043322ab8c9d
jiya <jiya@jiyas-MacBook-Air.local>
2026-04-18 12:02:21 +0530

    Add Vercel frontend to CORS

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ console.log('PORT:', process.env.PORT);
 
 // CORS configuration - FIXED
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:3000', 'https://findy-five.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
